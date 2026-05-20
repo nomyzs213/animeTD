8738890100f7256b263acf12e8f535e9c71a55bd
Szymon Zaremba <stary5717@gmail.com>
2026-05-20 09:40:37 +0000

    Co-authored-by: PiotrNiepsuj <PiotrNiepsuj@users.noreply.github.com>

---
+++ game/game.js
@@ -0,0 +1,2 @@
+
+document.getElementById("button").addEventListener("click", startGame);
---
+++ registration/register.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="register-style.css">
+</head>
+<body>
+    <div id="reg-box">
+      <label> podaj email <input type="email" class="reg-el" placholder="wpisz nazwe użytkownika"> </label>  
+
+     <label>  podaj nazwe użytkownika  <input type="" class="reg-el" placholder="podaj nazwe użytownika"> </label>
+
+     <label> podaj haslo <input type="password" class="reg-el" placeholder="********"> </label> 
+
+     <label>   powtóz hasło <input type="password" class="reg-el" placeholder="********"> </label>
+
+
+
+
+    </div>
+
+</body>
+</html>
